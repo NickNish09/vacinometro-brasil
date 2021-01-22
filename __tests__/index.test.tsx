7355d88f2f9df302a74f027d7b816758fa617fd9
Nicholas <nnmarques97@gmail.com>
2021-01-21 22:23:23 -0300

    feat: add brazil SVGmap (#2)
    * feat: add brazil SVGmap
    
    * style: add layout css to home page
    
    * test: add mock media

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import "../__mocks__/match-media";
 import HomePage from "../pages";
 
 describe("HomePage", () => {
-  it("should render the Vacinômetro title", () => {
+  it("should render the brazil map", () => {
     render(<HomePage />);
-    expect(screen.getByText("Vacinômetro")).toBeInTheDocument();
+    expect(screen.getByLabelText("Map of Brazil")).toBeInTheDocument();
   });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,36 @@
-const HomePage = () => <div>Vacinômetro</div>;
+import { useState } from "react";
+import { SVGMap } from "react-svg-map";
+import { Row, Col, Typography, Layout } from "antd";
+import Brazil from "../lib/custom-brazil.regions";
+
+const { Footer, Content } = Layout;
+const { Title } = Typography;
+
+const HomePage = () => {
+  const [district, setDistrict] = useState<string | null>("Brasil");
+  const districtName = (e: Event) =>
+    (e.target as HTMLElement).getAttribute("name");
+
+  return (
+    <Layout>
+      <Content className="container">
+        <Row>
+          <Col sm={24} md={12}>
+            <div className="map-container">
+              <SVGMap
+                map={Brazil}
+                onLocationClick={(e) => setDistrict(districtName(e))}
+              />
+            </div>
+          </Col>
+          <Col sm={24} md={12}>
+            <Title>Vacinômetro - {district}</Title>
+          </Col>
+        </Row>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+};
 
 export default HomePage;
