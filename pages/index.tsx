1373220d1773c95ff64c3e5791f8f8c1e0817e63
Nicholas Marques <nnmarques97@gmail.com>
2021-01-21 22:15:24 -0300

    style: add layout css to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { SVGMap } from "react-svg-map";
+import { Row, Col, Typography, Layout } from "antd";
 import Brazil from "../lib/custom-brazil.regions";
 
-const HomePage = () => (
-  <div>
-    <div className="map-container">
-      <SVGMap
-        map={Brazil}
-        onLocationClick={(e) =>
-          console.log((e.target as HTMLElement).getAttribute("name"))
-        }
-      />
-    </div>
-  </div>
-);
+const { Footer, Content } = Layout;
+const { Title } = Typography;
+
+const HomePage = () => {
+  const [district, setDistrict] = useState<string | null>("Brasil");
+  const districtName = (e: Event) =>
+    (e.target as HTMLElement).getAttribute("name");
+
+  return (
+    <Layout>
+      <Content className="container">
+        <Row>
+          <Col sm={24} md={12}>
+            <div className="map-container">
+              <SVGMap
+                map={Brazil}
+                onLocationClick={(e) => setDistrict(districtName(e))}
+              />
+            </div>
+          </Col>
+          <Col sm={24} md={12}>
+            <Title>Vacinômetro - {district}</Title>
+          </Col>
+        </Row>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+};
 
 export default HomePage;
