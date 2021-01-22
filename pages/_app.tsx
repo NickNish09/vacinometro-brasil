4023f82964d91781fc309f3f3023d4d3c73321b8
Nicholas Marques <nnmarques97@gmail.com>
2021-01-21 21:55:49 -0300

    feat: add brazil SVGmap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
+import "react-svg-map/lib/index.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
-const HomePage = () => <div>Vacinômetro</div>;
+import { SVGMap } from "react-svg-map";
+import Brazil from "../lib/custom-brazil.regions";
+
+const HomePage = () => (
+  <div>
+    <div className="map-container">
+      <SVGMap
+        map={Brazil}
+        onLocationClick={(e) =>
+          console.log((e.target as HTMLElement).getAttribute("name"))
+        }
+      />
+    </div>
+  </div>
+);
 
 export default HomePage;
