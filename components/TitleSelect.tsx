0cf827f9def7d026fd422598f6f7dc3de9b3103c
Nicholas Marques <nnmarques97@gmail.com>
2021-01-27 19:03:30 -0300

    feat: add dropdown selector and styles

--- components/TitleSelect.tsx
+++ components/TitleSelect.tsx
@@ -6,9 +6,11 @@ export type Props = {
   /* eslint-disable no-unused-vars */
   setDistrict: (name: string, id: string) => void;
   /* eslint-enable no-unused-vars */
+  districtName: string;
+  districtId: string;
 };
 
-const TitleSelect = ({ setDistrict }: Props) => {
+const TitleSelect = ({ setDistrict, districtName, districtId }: Props) => {
   const { Option } = Select;
 
   function handleChange(selectedOption: SelectValue) {
@@ -20,13 +22,22 @@ const TitleSelect = ({ setDistrict }: Props) => {
       labelInValue
       onChange={handleChange}
       className="district-select"
-      defaultValue={{ value: "todos", key: "todos", label: "todos" }}
+      defaultValue={{ value: "total", key: "total", label: "total" }}
+      value={{
+        value: districtName,
+        key: districtId,
+        label: `${districtName}`,
+      }}
     >
-      <Option value="todos" className="district-option">
-        Brasil (todos)
+      <Option value="total" className="district-option">
+        Brasil
       </Option>
       {brazilDistricts.map((district) => (
-        <Option value={district.stateId} className="district-option">
+        <Option
+          value={district.stateId}
+          className="district-option"
+          key={district.stateId}
+        >
           {`${district.name} (${district.stateId})`}
         </Option>
       ))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { SVGMap } from "react-svg-map";
 import { Row, Col, Typography, Layout } from "antd";
 import { GetStaticProps } from "next";
+import { MedicineBoxOutlined } from "@ant-design/icons";
 import Brazil from "../lib/custom-brazil.regions";
 import { DEFAULT_REVALIDATE_TIME } from "../utils/constants";
 import { District } from "../interfaces";
 import findDistrictById from "../utils/findDistrictById";
 import { getCases } from "../services/api/cases";
 import { formatedDate, formatedNumber } from "../utils/formaters";
+import TitleSelect from "../components/TitleSelect";
 
 const { Footer, Content } = Layout;
 const { Title } = Typography;
@@ -18,7 +20,7 @@ export type Props = {
 };
 
 const HomePage = ({ data, updatedAt }: Props) => {
-  const [districtName, setDistrictName] = useState<string | null>("Brasil");
+  const [districtName, setDistrictName] = useState<string>("Brasil");
   const [districtId, setDistrictId] = useState<string>("total");
 
   const getEventAttribute = (event: Event, attribute: string) =>
@@ -30,10 +32,9 @@ const HomePage = ({ data, updatedAt }: Props) => {
   };
 
   const setDistrictByEvent = (e: Event) => {
-    setDistrict(
-      getEventAttribute(e, "name") || "Brasil",
-      getEventAttribute(e, "id") || "total",
-    );
+    const name = getEventAttribute(e, "name") || "Brasil";
+    const id = getEventAttribute(e, "id") || "total";
+    setDistrict(`${name} (${id.toUpperCase()})`, id);
   };
 
   const districtData = findDistrictById(data, districtId);
@@ -47,23 +48,31 @@ const HomePage = ({ data, updatedAt }: Props) => {
               <div className="map-container">
                 <SVGMap
                   map={Brazil}
-                  onLocationClick={setDistrictByEvent}
-                  onLocationBlur={() => setDistrict("Brasil", "total")}
+                  onLocationFocus={setDistrictByEvent}
+                  // onLocationBlur={() => setDistrict("Brasil", "total")}
+                  isLocationSelected={(location) =>
+                    districtId === location.id.toUpperCase()
+                  }
                 />
               </div>
             </Col>
             <Col sm={24} md={12} xs={24}>
               <div className="data-container">
-                <Title>{districtName}</Title>
-                <Title level={3}>
-                  Vacinados: {formatedNumber(districtData.vaccinated)}
+                <TitleSelect
+                  setDistrict={setDistrict}
+                  districtId={districtId}
+                  districtName={districtName}
+                />
+                <Title level={3} className="vaccinated-text">
+                  <MedicineBoxOutlined /> Vacinados:{" "}
+                  {formatedNumber(districtData.vaccinated)}
                 </Title>
                 <p>Última atualização: {formatedDate(updatedAt)}</p>
               </div>
             </Col>
           </Row>
         </Content>
-        <Footer>
+        <Footer className="footer">
           Fonte:{" "}
           <a
             href="https://covid19br.wcota.me/"
