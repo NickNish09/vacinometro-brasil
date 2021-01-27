92f183289f2b1a41edec69fdce01e69fb50ac218
Nicholas Marques <nnmarques97@gmail.com>
2021-01-27 19:27:49 -0300

    style: add footer style and github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,14 +85,26 @@ const HomePage = ({ data, updatedAt }: Props) => {
           </Row>
         </Content>
         <Footer className="footer">
-          Fonte:{" "}
-          <a
-            href="https://covid19br.wcota.me/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://covid19br.wcota.me/
-          </a>{" "}
+          <p className="text-reference">
+            Fonte:{" "}
+            <a
+              href="https://covid19br.wcota.me/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              https://covid19br.wcota.me/
+            </a>
+          </p>
+          <br />
+          <p className="text-github">
+            <a
+              href="https://github.com/NickNish09/vacinometro-brasil"
+              target="_blank"
+              rel="noreferrer"
+            >
+              devnicknish © github
+            </a>
+          </p>
         </Footer>
       </Layout>
     );
